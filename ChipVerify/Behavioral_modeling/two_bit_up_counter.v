562e3bbeb1144bd11ac1edbb58c59e1a71e38dc7
aaron0730 <aaron0730@wolleytech.com>
2024-10-08 09:26:10 +0800

    gate level

--- ChipVerify/Behavioral_modeling/two_bit_up_counter.v
+++ ChipVerify/Behavioral_modeling/two_bit_up_counter.v
@@ -3,6 +3,7 @@ module design_top (input clk, input rstn, input en, output [1:0] out);
     counter #(.N(2)) u0 (.clk(clk), .rstn(rstn), .en(en));
 endmodule
 
+
 // 2-bit up-counter
 module counter #(parameter N = 2, parameter DOWN = 0)
                 (input clk, input rstn, input en, output reg [N-1:0] out);
