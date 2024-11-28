fd9b363cc82ca5a2d5cc406ad4026e466301043a
aron0730 <aron0730@gmail.com>
2024-11-29 00:10:15 +0800

    fork join

--- ChipVerify/System Verilog/Processes/SV_Thread.sv
+++ ChipVerify/System Verilog/Processes/SV_Thread.sv
@@ -90,3 +90,73 @@ endmodule
 [6 ns] Thread1: Orange is named after orange
 [7 ns] Thread2: But not anymore
 [11 ns] Thread3: Banana is a good fruit */
+
+
+
+// Nested fork join
+module tb;
+
+    initial begin
+        $display("[%0t] Main Thread: Fork join going to start", $time);
+        fork
+            fork
+                print(20, "Thread1_0");
+                print(30, "Thread1_1");
+            join
+            print(10, "Thread2");
+        join
+        $display("[%0t] Main Thread: Fork join has finished", $time);
+    end 
+
+    // Note that this task has to be automatic
+    task automatic print (int _time, string t_name);
+        #_time $display("[%0t] %s", $time, t_name);
+    endtask
+endmodule
+/* sim log :
+[0] Main Thread: Fork join going to start
+[10] Thread2
+[20] Thread1_0
+[30] Thread1_1
+[30] Main Thread: Fork join has finished */
+
+module tb;
+    initial begin
+        $display("[%0t] Main Thread: Fork join going to start", $time);
+
+        fork
+            // Thread 1
+            fork
+                #50 $display("[%0t] Thread1_0 ...",$time);
+                #70 $display("[%0t] Thread1_1 ...",$time);
+
+                begin
+                    #10 $display("[%0t] Thread1_2 ...", $time);
+                    #100 $display("[%0t] Thread1_2 finished", $time);
+                end
+
+            join
+
+            // Thread 2
+            begin
+                #5 $display("[%0t] Thread2 ...", $time);
+                #10 $display("[%0t] Thread2 finished", $time);
+            end
+
+            // Thread 3
+            #20 $display("[%0t] Thread3 finished", $time);
+        join
+        $display("[%0t] Main Thread: Fork join has finished", $time);
+    end
+endmodule
+/* sim log :
+[0] Main Thread: Fork join going to start
+[5] Thread2 ...
+[10] Thread1_2 ...
+[15] Thread2 finished
+[20] Thread3 finished
+[50] Thread1_0 ...
+[70] Thread1_1 ...
+[110] Thread1_2 finished
+[110] Main Thread: Fork join has finished */
+
