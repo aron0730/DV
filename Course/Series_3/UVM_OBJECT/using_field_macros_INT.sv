a56afb53341224baf0bb61ae6e2692a68acd89c8
aaron0730 <aaron0730@wolleytech.com>
2025-10-27 18:42:33 +0800

    add method of overriding do_print

--- Course/Series_3/UVM_OBJECT/using_field_macros_INT.sv
+++ Course/Series_3/UVM_OBJECT/using_field_macros_INT.sv
@@ -27,3 +27,40 @@ module tb;
         o.print();
     end
 endmodule
+
+//***************************
+
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
+// Object class is use to implement all the Dynamic Components of Testbench 
+// e.g. Transaction Class
+class obj extends uvm_object;
+    `uvm_object_utils(obj)
+
+    function new(string path = "OBJ");
+        super.new(path);
+    endfunction
+
+    rand bit [3:0] a;
+
+    virtual function void do_print(uvm_printer printer);
+        printer.print_string("==== Custom Print ====", "");
+        printer.print_field_int("a", a, 4, UVM_HEX);
+    endfunction
+
+    // `uvm_object_utils_begin(obj)
+        // `uvm_field_int(a, UVM_DEFAULT)
+    // `uvm_object_utils_end
+
+endclass
+
+module tb;
+    obj o;
+    initial begin
+        o = new("obj");
+        o.randomize();
+        // `uvm_info("TB_TOP", $sformatf("Value of a : %0d", o.a), UVM_NONE);
+        o.print();
+    end
+endmodule
