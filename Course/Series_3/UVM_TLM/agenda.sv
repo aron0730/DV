0328124b316c3fa015bf1c30da6b355a0a6e5e56
aaron0730 <aaron0730@wolleytech.com>
2025-12-24 15:51:33 +0800

    agenda, fundamentals

---
+++ Course/Series_3/UVM_TLM/agenda.sv
@@ -0,0 +1,9 @@
+TLM Communications
+Blocking and Non-blocking ports
+PUT port
+GET port
+Transport port
+Analysis Port
+TLM FIFO
+
+
---
+++ Course/Series_3/UVM_TLM/fundamentals.sv
@@ -0,0 +1,3 @@
+Sequencer -> Driver : Special TLM Port : SEQ_ITEM_PORT
+
+Monitor -> Scoreboard : TLP PORT : UVM_ANALYSIS PORT
