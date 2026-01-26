440ccb7ab6f91c12dbd0b2cbc352c2ef355cdefe
aaron0730 <aaron0730@wolleytech.com>
2026-01-26 20:40:42 +0800

    UART generator, receiver, transmitter

--- Course/Series_4/Verification_of_UART/clock_generator_for_different_baud/tb.sv
+++ Course/Series_4/Verification_of_UART/clock_generator_for_different_baud/tb.sv
@@ -287,25 +287,20 @@ module tb;
     clk_if vif();
     clk_gen dut (.clk(vif.clk),.rst(vif.rst), .baud(vif.baud), .tx_clk(vif.tx_clk));
   
-    
     initial begin
         vif.clk <= 0;
     end
     
     always #10 vif.clk <= ~vif.clk; //1/50 20nsec 10nsec
     
-    
-    
     initial begin
         uvm_config_db#(virtual clk_if)::set(null, "*", "vif", vif);
         run_test("test");
     end
     
-    
     initial begin
         $dumpfile("dump.vcd");
         $dumpvars;
     end
-    
     
 endmodule
