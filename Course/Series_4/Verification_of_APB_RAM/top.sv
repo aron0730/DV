64e65f7e807aef1d569dc354000971610f86c9ae
aaron0730 <aaron0730@wolleytech.com>
2026-03-18 19:11:17 +0800

    start to implement APB verification

---
+++ Course/Series_4/Verification_of_APB_RAM/top.sv
@@ -0,0 +1,84 @@
+module apb_ram (
+    input presetn,
+    input pclk,
+    input psel,
+    input penable,
+    input pwrite,
+    input [31:0] paddr, pwdata,
+    output reg [31:0] prdata,
+    output reg pready, pslverr
+);
+    reg [31:0] mem[32];
+
+    typedef enum {idle = 0, setup = 1, access = 2, transfer = 3} state_type;
+
+    state_type state = idle;
+
+    always@(posedge pclk) begin
+        if(presetn == 1'b0) begin  // active low
+            state <= idle;
+            prdata <= 32'h0000_0000;
+            pready <= 1'b0;
+            pslverr <= 1'b0;
+
+            for(int i = 0; i < 32; i++) begin
+                mem[i] <= 0;
+            end
+        end
+        else begin
+            case(state)
+                idle: begin
+                    prdata <= 32'h0000_0000;
+                    pready <= 0;
+                    penable <= 0;
+                    state <= setup;
+                end
+
+                setup: begin  // start of transaction
+                    if(psel == 1'b1)
+                        state <= access;
+                    else
+                        state <= setup;
+                end
+
+                access: begin
+                    if(pwrite && penable) begin
+                        if(paddr < 32) begin
+                            mem[addr] <= pwdata;
+                            state <= transfer;
+                            pslverr <= 1'b0;
+                            pready <= 1'b1;
+                        end 
+                        else begin
+                            state <= transfer;
+                            pready <= 1'b1;
+                            pslverr <= 1'b1;
+                        end
+                    end
+                    else(!pwrite && penable) begin
+                        if(paddr < 32) begin
+                            prdata <= mem[paddr];
+                            state <= transfer;
+                            pready <= 1'b1;
+                            pslverr <= 1'b0;
+                        end
+                        else begin
+                            state <= transfer;
+                            pready <= 1'b1;
+                            pslverr <= 1'b1;
+                            prdata <= 32'hxxxx_xxxx;
+                        end
+                    end
+                end
+
+                transfer: begin
+                    state <= setup;
+                    pready <= 1'b0;
+                    pslverr <= 1'b0;
+                end
+
+                default: state <= idle;
+            endcase
+        end
+    end
+endmodule
